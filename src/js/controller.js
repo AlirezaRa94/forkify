5d4a6bc262cba23bee47087d08b60426a0820bc5
Alireza Raei <alirezaraei17@gmail.com>
2024-02-29 21:22:11 +1100

    feat(UI): Add loading spinner to improve UX
    - Introduces loading spinner with icon to visually indicate loading state.
    - Includes CSS animation for rotation effect.

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,8 +2,22 @@ import icons from '../img/icons.svg';
 
 const recipeContainer = document.querySelector('.recipe');
 
+const renderSpinner = function (parentEl) {
+  const markup = `
+    <div class="spinner">
+      <svg>
+        <use href="${icons}#icon-loader"></use>
+      </svg>
+    </div>
+  `;
+  parentEl.innerHTML = '';
+  parentEl.insertAdjacentHTML('afterbegin', markup);
+};
+
 const showRecipe = async function () {
   try {
+    // Rendering spinner
+    renderSpinner(recipeContainer);
     // Loading recipe
     const res = await fetch(
       'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'

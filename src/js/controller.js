fa158935f1ac94c3950fff8653f2852da22636c4
Alireza Raei <alirezaraei17@gmail.com>
2024-03-02 16:07:17 +1100

    refactor: Improve error handling and fetch abstraction
    - Wrap controlRecipes function in controller.js in a try-catch block
    - Create a new getJSON function in a new helpers.js file for fetching and error handling
    - Use this new getJSON function in model.js
    - Change temporary error handling in model.js to console.error

--- src/js/controller.js
+++ src/js/controller.js
@@ -5,17 +5,21 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
 const controlRecipes = async function () {
-  const id = window.location.hash.slice(1);
-  if (!id) return;
+  try {
+    const id = window.location.hash.slice(1);
+    if (!id) return;
 
-  // Rendering spinner
-  recipeView.renderSpinner();
+    // Rendering spinner
+    recipeView.renderSpinner();
 
-  // Loading recipe
-  await model.loadRecipe(id);
+    // Loading recipe
+    await model.loadRecipe(id);
 
-  // Rendering recipe
-  recipeView.render(model.state.recipe);
+    // Rendering recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    alert(err);
+  }
 };
 
 ['hashchange', 'load'].forEach(ev =>

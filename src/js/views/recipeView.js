c3ea7a21665298b01cce58f7eabd9c1cb0bdcb4a
Alireza Raei <alirezaraei17@gmail.com>
2024-03-03 19:36:16 +1100

    style: Adjust UI elements and styles
    - Removed user-generated icon from RecipeView and ResultsView.
    - Added 'btn--bookmark' class to bookmark button in RecipeView for styling.
    - Adjusted link active state in ResultsView.
    - Added new CSS rule for 'btn--bookmark' in _components.scss.

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -69,12 +69,7 @@ class RecipeView extends View {
           </div>
         </div>
 
-        <div class="recipe__user-generated">
-          <svg>
-            <use href="${icons}#icon-user"></use>
-          </svg>
-        </div>
-        <button class="btn--round">
+        <button class="btn--round btn--bookmark">
           <svg class="">
             <use href="${icons}#icon-bookmark-fill"></use>
           </svg>

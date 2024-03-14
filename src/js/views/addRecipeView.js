252300edb685bc0dbf8374a228a8b97e6c17cfad
Alireza Raei <alirezaraei17@gmail.com>
2024-03-15 06:45:09 +1100

    feat: Add AddRecipeView class with form open/close handlers
    - Created a new AddRecipeView class in addRecipeView.js. This class extends the View class and includes methods to open and close the recipe addition form.
    - The AddRecipeView class includes two handler functions, _addHandlerOpenWindow and _addHandlerCloseWindow, which add event listeners to open and close the form.
    - Integrated the AddRecipeView class into the controller by importing it in controller.js.

---
+++ src/js/views/addRecipeView.js
@@ -0,0 +1,33 @@
+import View from './view';
+
+class AddRecipeView extends View {
+  _parentElement = document.querySelector('.upload');
+  _window = document.querySelector('.add-recipe-window');
+  _overlay = document.querySelector('.overlay');
+  _btnOpen = document.querySelector('.nav__btn--add-recipe');
+  _btnClose = document.querySelector('.btn--close-modal');
+
+  constructor() {
+    super();
+    this._addHandlerOpenWindow();
+    this._addHandlerCloseWindow();
+  }
+
+  toggleWindow() {
+    this._overlay.classList.toggle('hidden');
+    this._window.classList.toggle('hidden');
+  }
+
+  _addHandlerOpenWindow() {
+    this._btnOpen.addEventListener('click', this.toggleWindow.bind(this));
+  }
+
+  _addHandlerCloseWindow() {
+    this._btnClose.addEventListener('click', this.toggleWindow.bind(this));
+    this._overlay.addEventListener('click', this.toggleWindow.bind(this));
+  }
+
+  _genenerateMarkup() {}
+}
+
+export default new AddRecipeView();

bfb03b54db7ebe49990b3baaaeae631134cc973a
Alireza Raei <alirezaraei17@gmail.com>
2024-03-15 14:52:13 +1100

    feat: Improve user feedback for recipe upload
    - Added a new constant, MODAL_CLOSE_SEC, to config.js. This constant determines how long the success message is displayed after a recipe is uploaded.
    - Updated controlAddRecipe in controller.js to show a loading spinner while the recipe is being uploaded, display a success message after the recipe is uploaded, and automatically close the form window after a delay.
    - Added a _message property to AddRecipeView in addRecipeView.js. This property is used to display a success message after a recipe is uploaded.

--- src/js/config.js
+++ src/js/config.js
@@ -4,3 +4,4 @@ export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 12;
 export const API_KEY = process.env.API_KEY;
+export const MODAL_CLOSE_SEC = 2.5;

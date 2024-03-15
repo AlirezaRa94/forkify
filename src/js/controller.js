19583c78c95ffb1025760fdc434d37f01c8fd415
Alireza Raei <alirezaraei17@gmail.com>
2024-03-15 16:39:31 +1100

    refactor: Adjust sequence of operations after recipe upload
    - In controlAddRecipe function in controller.js, moved the rendering of the bookmarks view after the URL is updated with the new recipe ID. This ensures that the URL is updated before the bookmarks view is rendered, which may depend on the new URL.

--- src/js/controller.js
+++ src/js/controller.js
@@ -103,11 +103,11 @@ const controlAddRecipe = async function (newRecipe) {
     // Success message
     addRecipeView.renderMessage();
 
-    // Render bookmark view
-    bookmarksView.render(model.state.bookmarks);
-
     // Change ID in URL
     window.history.pushState(null, '', `#${model.state.recipe.id}`);
+
+    // Render bookmark view
+    bookmarksView.render(model.state.bookmarks);
 
     // Close form window
     setTimeout(function () {

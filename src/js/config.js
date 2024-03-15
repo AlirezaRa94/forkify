8249529db810e73e6283bbafae53b4a66b3db053
Alireza Raei <alirezaraei17@gmail.com>
2024-03-15 13:21:26 +1100

    feat: Integrate dotenv for environment variable management
    - Added dotenv as a dependency in package.json and package-lock.json.
    - Imported dotenv/config in config.js to load environment variables from a .env file.
    - Added .env to .gitignore to prevent environment variables from being committed to the repository.
    - Added API_KEY to config.js, which is loaded from the environment variables.

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,6 @@
+import 'dotenv/config';
+
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 12;
+export const API_KEY = process.env.API_KEY;
